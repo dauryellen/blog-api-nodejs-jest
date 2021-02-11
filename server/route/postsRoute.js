8456fd21225d69383af23d5bc15e28f93da77a9c
dauryellen <dauryellen@live.com>
2021-02-11 15:01:59 -0300

    conectando rotas ao server

--- server/route/postsRoute.js
+++ server/route/postsRoute.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/posts");
-router.get("/posts/:id");
-router.post("/posts");
-router.put("/posts/:id");
-router.delete("/posts/:id");
+router.get("/posts", async function (req, res) {
+  res.json([
+    {
+      id: 1,
+      title: "REST API: Métodos",
+      content: "...",
+      date: new Date(),
+    },
+  ]);
+});
+router.get("/posts/:id", async function (req, res) {});
+router.post("/posts", async function (req, res) {});
+router.put("/posts/:id", async function (req, res) {});
+router.delete("/posts/:id", async function (req, res) {});
 
 module.exports = router;
