7f4f66c9fb62a014a24ab137e257013322058d03
dauryellen <dauryellen@live.com>
2021-02-11 14:29:15 -0300

    criação das primeiras rotas

--- server/route/postsRoute.js
+++ server/route/postsRoute.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/posts");
+router.get("/posts/:id");
+router.post("/posts");
+router.put("/posts/:id");
+router.delete("/posts/:id");
+
 module.exports = router;
